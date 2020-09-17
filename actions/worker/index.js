91f4e6c64ed4292960e89909a8ce30d8e948de4e
David Gonzalez <davidjgonzalez@gmail.com>
2020-09-17 10:15:36 -0400

    Initial commit

---
+++ actions/worker/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Jimp = require('jimp');
+const { worker, SourceCorruptError, ClientError } = require('@adobe/asset-compute-sdk');
+const fs = require('fs').promises;
+
+exports.main = worker(async (source, rendition, params) => {
+    const stats = await fs.stat(source.path);
+    if (stats.size === 0) {
+        throw new SourceCorruptError('source file is empty');
+    }
+
+    // Read/parse and validate parameters
+    const SIZE = parseInt(rendition.instructions.size) || 800; 
+    const CONTRAST = parseFloat(rendition.instructions.contrast) || 0;
+    const BRIGHTNESS = parseFloat(rendition.instructions.brightness) || 0;
+    
+    // Example of console.log(..) logging
+    console.log("Size: " + SIZE);
+    console.log("Contrast: " + CONTRAST);
+    console.log("Brightness: " + BRIGHTNESS);
+
+    if (SIZE <= 10 || SIZE >= 10000) {
+        throw new RenditionInstructionsError("'size' must be between 10 and 1,0000");
+    } else if (CONTRAST <= -1 || CONTRAST >= 1) {
+        throw new RenditionInstructionsError("'contrast' must between -1 and 1");
+    } else if (BRIGHTNESS <= -1 || BRIGHTNESS >= 1) {
+        throw new RenditionInstructionsError("'brightness' must between -1 and 1");
+    }
+
+    // Create target rendition image 
+    let renditionImage =  new Jimp(SIZE, SIZE, 0x0);
+
+    // Read and perform transformations on the source binary image
+    let image = await Jimp.read(source.path);
+
+    // Crop a circle from the source asset, and then apply contrast and brightness
+    image.crop(
+            image.bitmap.width < image.bitmap.height ? 0 : (image.bitmap.width - image.bitmap.height) / 2,
+            image.bitmap.width < image.bitmap.height ? (image.bitmap.height - image.bitmap.width) / 2 : 0,
+            image.bitmap.width < image.bitmap.height ? image.bitmap.width : image.bitmap.height,
+            image.bitmap.width < image.bitmap.height ? image.bitmap.width : image.bitmap.height
+        )   
+        .circle()
+        .scaleToFit(SIZE, SIZE)
+        .contrast(CONTRAST)
+        .brightness(BRIGHTNESS);
+
+    // Place the transformed image onto the transparent renditionImage to save as PNG
+    renditionImage.composite(image, 0, 0)
+
+    // Write the final transformed image to the asset's rendition
+    renditionImage.writeAsync(rendition.path);
+});
+
+// Custom error used for renditions.instructions parameter checking
+class RenditionInstructionsError extends ClientError {
+    constructor(message) {
+        super(message, "RenditionInstructionsError", "rendition_instructions_error");
+        Error.captureStackTrace(this, RenditionInstructionsError);
+    }
+}
