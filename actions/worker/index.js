89d6ac53c23ffaafe838191ecf92a67578476682
David Gonzalez <davidjgonzalez@gmail.com>
2020-09-24 13:48:39 -0400

    Adobe AL2 Copyright

--- actions/worker/index.js
+++ actions/worker/index.js
@@ -1,3 +1,17 @@
+// Copyright 2020 Adobe
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
 const Jimp = require('jimp');
@@ -50,7 +64,7 @@ exports.main = worker(async (source, rendition, params) => {
     renditionImage.composite(image, 0, 0)
 
     // Write the final transformed image to the asset's rendition
-    renditionImage.writeAsync(rendition.path);
+    await renditionImage.writeAsync(rendition.path);
 });
 
 // Custom error used for renditions.instructions parameter checking
