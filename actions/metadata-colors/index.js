b8924f843983969d1830a8850409cd229e68f188
David Gonzalez <davidjgonzalez@gmail.com>
2020-11-03 14:51:20 -0500

    Asset Compute metadata worker (metadata-colors)

---
+++ actions/metadata-colors/index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const { worker, SourceCorruptError } = require("@adobe/asset-compute-sdk");
+const fs = require("fs").promises;
+
+// Require the @adobe/asset-compute-xmp module to create XMP 
+const { serializeXmp } = require("@adobe/asset-compute-xmp");
+
+// Require supporting npm modules to derive image colors from image data
+const getColors = require("get-image-colors");
+// Require supporting npm modules to convert image colors to color names
+const namer = require("color-namer");
+
+exports.main = worker(async (source, rendition, params) => {
+  // Perform any necessary source (input) checks
+  const stats = await fs.stat(source.path);
+  if (stats.size === 0) {
+    // Throw appropriate errors whenever an erring condition is met
+    throw new SourceCorruptError("source file is empty");
+  }
+  const MAX_COLORS = 10;
+  const DEFAULT_COLORS_FAMILY = 'basic';
+
+  // Read the color family parameter to use to derive the color names
+  let colorsFamily = rendition.instructions.colorsFamily || DEFAULT_COLORS_FAMILY;
+
+  if (['basic', 'html', 'ntc', 'pantone', 'roygbiv', 'hex'].indexOf(colorsFamily) === -1) { 
+      colorsFamily = DEFAULT_COLORS_FAMILY;
+  }
+  
+  // Use the `get-image-colors` module to derive the most common colors from the image
+  let colors = await getColors(source.path, { options: MAX_COLORS });
+
+  // Convert the color Chroma objects to names
+  let colorNames = colors.map((color) => getColorName(colorsFamily, color));
+
+  // Serialize the data to XMP metadata
+  // These properties will be written to the [dam:Asset]/jcr:content/metadata resource
+  const xmp = serializeXmp({
+      "wknd:colors": [...new Set(colorNames)],
+      "wknd:colorsFamily": colorsFamily
+    }, {
+      // Define any property namespaces in the above property/value definition
+      namespaces: {
+        wknd: "https://ns.wknd.site/1.0/schema.xml",
+      },
+    }
+  );
+
+  // Save the XMP metadata to be written back to the asset's metadata node
+  await fs.writeFile(rendition.path, xmp, "utf-8");
+});
+
+/**
+ * Helper function that derives the closest color name for the color, based on the colors family
+ * 
+ * @param {*} colorsFamily the colors name family to use
+ * @param {*} color the color to convert to a name
+ */
+function getColorName(colorsFamily, color) {
+    if ('hex' === colorsFamily) {  return color; }
+
+    let names = namer(color.rgb())[colorsFamily];
+
+    if (names.length >= 1) { return names[0].name; }
+}
